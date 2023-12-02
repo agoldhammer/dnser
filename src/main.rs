eacdf0c9823feb36b24d9be41e560b8c82ce3240
Art Goldhammer <art.goldhammer@gmail.com>
2023-12-02 17:11:48 -0500

    working on single ip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// use std::fs::File;
+// use std::io::{self, BufRead};
+// use std::path::Path;
 use std::net::IpAddr;
 use std::time::Duration;
 use tokio::time::timeout;
@@ -8,19 +11,27 @@ use hickory_resolver::AsyncResolver;
 
 #[tokio::main]
   async fn main() {
+    
+
     let resolver = AsyncResolver::tokio_from_system_conf().unwrap();
     let ip_addr: IpAddr = "8.8.8.8".parse().unwrap();
 
     let reverse_lookup = resolver.reverse_lookup(ip_addr);
-    let timeout_duration = Duration::from_secs(5);
+    let timeout_duration = Duration::from_millis(150);
     let result = timeout(timeout_duration, reverse_lookup).await;
 
     match result {
         Ok(Ok(lookup_result)) => {
+            
+            for z in lookup_result.iter() {
+                println!("host: {}", z);
+            }
+            
             // for rdata in lookup_result.iter().filter_map(RData::PTR) {
-                // println!("PTR: {}", rdata.ptrdname());
-                dbg!("result: {}", lookup_result);
+            //     println!("PTR: {}", rdata.ptrdname());
             // }
+            // dbg!(y);
+            // dbg!(lookup_result);
         }
         Ok(Err(err)) => println!("Failed to find PTR record: {}", err),
         Err(_) => println!("Reverse lookup timed out"),
