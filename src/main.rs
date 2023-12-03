95b5f3e8be722156e41e114bffea6336979aad92
Art Goldhammer <art.goldhammer@gmail.com>
2023-12-03 10:11:17 -0500

    working

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ use std::error::Error;
 use std::net::IpAddr;
 use std::time::Duration;
 use tokio::time::timeout;
+use tokio::task::JoinSet;
 use hickory_resolver::TokioAsyncResolver;
-use hickory_resolver::error::ResolveError;
-use tokio::time::error::Elapsed;
-use hickory_resolver::lookup::ReverseLookup;
+// use hickory_resolver::error::ResolveError;
+// use tokio::time::error::Elapsed;
+// use hickory_resolver::lookup::ReverseLookup;
 // use hickory_resolver::config::\*;
 // use hickory_resolver::proto::rr::RData;
 // use hickory_resolver::proto::rr::RecordType;
@@ -19,15 +20,25 @@ fn read_lines(path: &PathBuf) -> Result<io::Lines<BufReader<File>>, Box<dyn Erro
     return Ok(io::BufReader::new(file).lines());
 }
 
-async fn get_name(ip_str: &String)  -> Result<Result<ReverseLookup, ResolveError>, Elapsed> {
+async fn get_name(ip_str: &String)  -> () {
 // async fn get_name(ip_str: &String)  . {
-    const TIMEOUT_MS: u64 = 150;
+    const TIMEOUT_MS: u64 = 1500;
     let ip_addr: IpAddr = ip_str.parse().unwrap();
     let resolver = TokioAsyncResolver::tokio_from_system_conf().unwrap();
 
     let reverse_lookup = resolver.reverse_lookup(ip_addr);
     let timeout_duration = Duration::from_millis(TIMEOUT_MS);
-    timeout(timeout_duration, reverse_lookup).await
+    let lookup_result = timeout(timeout_duration, reverse_lookup).await;
+    match lookup_result {
+                Ok(Ok(lookup_result)) => {
+                    for record in lookup_result.iter() {
+                        println!("ip: {}: host: {}", ip_str, record);
+                    }
+                }
+                Ok(Err(_)) => println!("{}: No PTR record", ip_str),
+                Err(_) => println!("ip: {}: timed out", ip_str),
+        };
+        ()
 }
 
 #[tokio::main]
@@ -35,26 +46,13 @@ async fn main() {
     let mut fpath: PathBuf = PathBuf::new();
     fpath.push("unique_ips_54.txt");
     let lines = read_lines(&fpath).unwrap();
+    let mut set = JoinSet::new();
     
     for ip_str in lines {
-        let lookup_result = get_name(&ip_str.unwrap()).await;
-        // dbg!(lookup_result);
-            match lookup_result {
-                Ok(Ok(lookup_result)) => {
-                    
-                    for record in lookup_result.iter() {
-                        println!("host: {}", record);
-
-                    }
-                    
-                    // for rdata in lookup_result.iter().filter_map(RData::PTR) {
-                    //     println!("PTR: {}", rdata.ptrdname());
-                    // }
-                    // dbg!(y);
-                    // dbg!(lookup_result);
-                }
-                Ok(Err(err)) => println!("Failed to find PTR record: {}", err),
-                Err(_) => println!("Reverse lookup timed out"),
-        }
+        set.spawn(async move {get_name(&ip_str.unwrap()).await});
+            
     }
+    while let Some(res) = set.join_next().await {
+        res.expect("join error");
+    };
 }
